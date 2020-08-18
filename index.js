5f23403ee82cc77c427625ef325b122fe49dbe5f
Rodrigo Cardoso da luz <diazrma@gmail.com>
2020-08-17 23:12:42 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+
+const client = new Discord.Client();
+
+client.on('message', message => {
+	if (message.content === '!play') {
+		if (message.channel.type === 'dm') return;
+
+		const voiceChannel = message.member.voice.channel;
+
+		if (!voiceChannel) {
+			return message.reply('please join a voice channel first!');
+		}
+
+		voiceChannel.join().then(connection => {
+			const stream = ytdl('https://www.youtube.com/watch?v=D57Y1PruTlw', { filter: 'audioonly' });
+			const dispatcher = connection.play(stream);
+
+			dispatcher.on('finish', () => voiceChannel.leave());
+		});
+	}
+});
+
+client.login('NzQ1MDcyNjAxMTIxOTQ3NzIw.Xzsc7g.0lgG_weyKi6saFsLWxewt-UTrTE');
